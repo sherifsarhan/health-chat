8b2ccab9deba8a96f32928cd3495d11d1b8b1e78
sherifsarhan <sherif.a.sarhan@gmail.com>
2017-10-23 17:02:05 -0400

    schedule expansion

--- schedule.js
+++ schedule.js
@@ -22,11 +22,44 @@ module.exports = {
                     },
                     24: {
                         8: {
-                            0: 'booked',
+                            0: 'available',
+                            30: 'booked'
+                        },
+                        9: {
+                            0: 'available'
+                        },
+                        10: {
+                            0: 'available',
                             30: 'available'
                         },
-                        11: {
+                        15: {
+                            0: 'booked'
+                        }
+                    },
+                    25: {
+                        14: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
                             0: 'available'
+                        },
+                        16: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        17: {
+                            0: 'available'
+                        }
+                    },
+                    26: {
+                        11: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        12: {
+                            0: 'available',
+                            30: 'available'
                         },
                         13: {
                             0: 'booked',
@@ -34,6 +67,38 @@ module.exports = {
                         },
                         15: {
                             0: 'available'
+                        }
+                    },
+                    27: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    28: {
+                        8: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        11: {
+                            0: 'booked'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'booked'
                         }
                     }
                 }
