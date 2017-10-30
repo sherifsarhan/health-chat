1abd488984fa103c0f11cf7aa20dc8511f53b2dc
sherifsarhan <sherif.a.sarhan@gmail.com>
2017-10-30 17:26:51 -0400

    duplicate schedule for all doctors

--- schedule.js
+++ schedule.js
@@ -102,6 +102,516 @@ module.exports = {
                         15: {
                             0: 'available'
                         }
+                    },
+                    5: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    6: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    7: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    }                
+                }
+            }
+        },
+        Psychiatrist: {
+            2017: {
+                9: {
+                    30: {
+                        9: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        10: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        14: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    31: {
+                        8: {
+                            0: 'available',
+                            30: 'booked'
+                        },
+                        9: {
+                            0: 'available'
+                        },
+                        10: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'booked'
+                        }
+                    }    
+                },
+                10: {
+                    1: {
+                        8: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        11: {
+                            0: 'booked'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'booked'
+                        }
+                    },
+                    2: {
+                        14: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        },
+                        16: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        17: {
+                            0: 'available'
+                        }
+                    },
+                    3: {
+                        11: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        12: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        13: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    4: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    5: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    6: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    7: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    }                
+                }
+            }
+        },
+        Dermatologist: {
+            2017: {
+                9: {
+                    30: {
+                        9: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        10: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        14: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    31: {
+                        8: {
+                            0: 'available',
+                            30: 'booked'
+                        },
+                        9: {
+                            0: 'available'
+                        },
+                        10: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'booked'
+                        }
+                    }    
+                },
+                10: {
+                    1: {
+                        8: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        11: {
+                            0: 'booked'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'booked'
+                        }
+                    },
+                    2: {
+                        14: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        },
+                        16: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        17: {
+                            0: 'available'
+                        }
+                    },
+                    3: {
+                        11: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        12: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        13: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    4: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    5: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    6: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    7: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    }                
+                }
+            }
+        },
+        Cardiologist: {
+            2017: {
+                9: {
+                    30: {
+                        9: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        10: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        14: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    31: {
+                        8: {
+                            0: 'available',
+                            30: 'booked'
+                        },
+                        9: {
+                            0: 'available'
+                        },
+                        10: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'booked'
+                        }
+                    }    
+                },
+                10: {
+                    1: {
+                        8: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        11: {
+                            0: 'booked'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'booked'
+                        }
+                    },
+                    2: {
+                        14: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        },
+                        16: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        17: {
+                            0: 'available'
+                        }
+                    },
+                    3: {
+                        11: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        12: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        13: {
+                            0: 'booked',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    4: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    5: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    6: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
+                    },
+                    7: {
+                        8: {
+                            0: 'booked',
+                            30: 'booked'
+                        },
+                        11: {
+                            0: 'available'
+                        },
+                        13: {
+                            0: 'available',
+                            30: 'available'
+                        },
+                        15: {
+                            0: 'available'
+                        }
                     }                
                 }
             }
